27987815ad0f207f50f2d7d78cb1ed381d48db7d
erenc1ftc <meren228228@icloud.com>
2024-10-01 02:49:59 +0300

    İlk commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "eren1234",
+        database: "blogdb"
+    },
+    email: {
+        username: "kolsuzdana@hotmail.com",
+        password: "Erenjuke2.",
+        from: "kolsuzdana@hotmail.com"
+    }
+}
+
+module.exports = config;
